0240360857bbf6057e6bb6cf2ee9e6fffb2d6bdf
Sheyda Rezaei <sheydarezaee@gmail.com>
2017-07-06 18:06:38 -0700

    'adds hir-rating feature and stores the student hir rating info to the database' : This is the 1st commit message: renders closed tickets to students view
    adds rating component and handleRatingClick function
    
    added Feedback and related event handlers
    
    revises css on the feedback page
    
    updates the database with rating and review on the latest closed ticket
    
    resolved conflicts on sampleData
    
    stores rating star and reveiw into database
    
    'styling on seatingchart and feedback modals'  This is a combination of 2 commits.
    
    resolves bug on seatingchart
    
    matches styling for feedback and seatingchart modals

--- client/components/ticketEntry.jsx
+++ client/components/ticketEntry.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import moment from 'moment';
+// import io from 'socket.io-client';
 
 class TicketEntry extends React.Component {
   constructor(props) {
@@ -33,7 +34,7 @@ class TicketEntry extends React.Component {
   }
 
   claimTicket(){
-    this.props.updateTickets({ id: this.props.ticket.id, status: 'Claimed' })
+    this.props.updateTickets({ id: this.props.ticket.id, status: 'Claimed' });
   }
 
   render() {
