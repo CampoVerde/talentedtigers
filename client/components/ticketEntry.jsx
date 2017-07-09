56cb3746562d3a63c58b0699db189d4c57764e43
Sheyda Rezaei <sheydarezaee@gmail.com>
2017-07-09 15:26:38 -0700

    'removes dump.rdb and line four of ticketEnrty.jsx' This is a combination of 2 commits.
    removes dump.rdb file and line 4 of ticketEntry.jsx
    
    removes dump.rdb and removes line four of ticketEntry.jsx

--- client/components/ticketEntry.jsx
+++ client/components/ticketEntry.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import moment from 'moment';
-// import io from 'socket.io-client';
+
 
 class TicketEntry extends React.Component {
   constructor(props) {
